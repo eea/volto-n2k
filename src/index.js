8f1cfeaa8d1686f02a6b65d2b74064e341f8139a
razvanMiu <miu.razvan28@gmail.com>
2021-03-16 19:06:53 +0200

    Scroll to hash on mount

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import installExplodedPiesChart from './components/manage/Blocks/ExplodedPiesChart';
 
 import { Header, Footer } from '@eeacms/volto-n2k/components';
+import { withScrollToHash } from '@eeacms/volto-n2k/hocs';
 
 import './styles.less';
 
@@ -15,6 +16,16 @@ const applyConfig = (config) => {
       Breadcrumbs: () => <></>,
     },
   };
+
+  config.settings.appExtras = [
+    ...(config.settings.appExtras || []),
+    {
+      match: '',
+      component: withScrollToHash(() => {
+        return <React.Fragment />;
+      }),
+    },
+  ];
 
   return [installExplodedPiesChart].reduce((acc, apply) => apply(acc), config);
 };
