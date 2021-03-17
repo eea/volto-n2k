f8921b6e0ab0cbf39c9ea4e048370e5d432d38a3
razvanMiu <miu.razvan28@gmail.com>
2021-03-17 18:42:44 +0200

    Moved hashlink logic to volto-slate

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import installExplodedPiesChart from './components/manage/Blocks/ExplodedPiesChart';
 
 import { Header, Footer } from '@eeacms/volto-n2k/components';
-import { withScrollToHash } from '@eeacms/volto-n2k/hocs';
 
 import './styles.less';
 
@@ -16,16 +15,6 @@ const applyConfig = (config) => {
       Breadcrumbs: () => <></>,
     },
   };
-
-  config.settings.appExtras = [
-    ...(config.settings.appExtras || []),
-    {
-      match: '',
-      component: withScrollToHash(() => {
-        return <React.Fragment />;
-      }),
-    },
-  ];
 
   return [installExplodedPiesChart].reduce((acc, apply) => apply(acc), config);
 };
