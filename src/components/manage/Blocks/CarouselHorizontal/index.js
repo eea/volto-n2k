09baad72af985d7895cfd250fec40db8f3748dc5
Miu Razvan <miu.razvan28@gmail.com>
2022-08-23 11:14:41 +0300

    Change style for arrows and dots in carousel

--- src/components/manage/Blocks/CarouselHorizontal/index.js
+++ src/components/manage/Blocks/CarouselHorizontal/index.js
@@ -1,3 +1,4 @@
+import DefaultHorizontalView from './DefaultHorizontalView';
 import HorizontalCarouselView from './HorizontalView';
 import carouselSchema from './schema';
 
@@ -8,6 +9,10 @@ export default (config) => {
     ...(config.blocks.blocksConfig.tabs_block || {}),
     templates: {
       ...(config.blocks.blocksConfig.tabs_block?.templates || {}),
+      carousel: {
+        ...(config.blocks.blocksConfig.tabs_block?.templates?.carousel || {}),
+        view: DefaultHorizontalView,
+      },
       carousel_n2k: {
         title: 'Carousel Natura 2000',
         view: HorizontalCarouselView,
