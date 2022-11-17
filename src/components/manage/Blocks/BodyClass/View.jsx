747144ac9d7deeff8b5924d3ee23fafc5bdf91ae
Nilesh <nileshgulia@gmail.com>
2022-11-17 14:07:12 +0530

    Update View.jsx

--- src/components/manage/Blocks/BodyClass/View.jsx
+++ src/components/manage/Blocks/BodyClass/View.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { BodyClass } from '@plone/volto/helpers';
 
 const View = (props) => {
+  const { data } = props;
   return (
     <>
       {props.mode === 'edit' ? <p>Body className: {props.data.class}</p> : ''}
-      <BodyClass className={props.data.class.trim() || ''} />
+      <BodyClass className={data.class ? data.class.trim() : ''} />
     </>
   );
 };
