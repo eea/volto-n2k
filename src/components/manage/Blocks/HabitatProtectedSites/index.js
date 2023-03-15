b2262fcb7416208ba3ce5368bbd50bda2e0e58f6
Claudia Ifrim <ifrim.claudia@gmail.com>
2023-03-15 10:24:18 +0200

    update layer for species and habitats

--- src/components/manage/Blocks/HabitatProtectedSites/index.js
+++ src/components/manage/Blocks/HabitatProtectedSites/index.js
@@ -3,7 +3,7 @@ import getSchema from './schema';
 
 export function getHabitatProtectedSitesURL(code_2000) {
   return encodeURI(
-    `https://bio.discomap.eea.europa.eu/arcgis/rest/services/ProtectedSites/EUNIS_Website_Dyna_WM/MapServer/1/query?f=json&where=HABITATCODE LIKE '%${code_2000.toUpperCase()}%'&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=SPECIESCODE,SITECODE,HABITATCODE,OBJECTID&outSR=102100`,
+    `https://bio.discomap.eea.europa.eu/arcgis/rest/services/ProtectedSites/EUNIS_Website_Dyna_WM/MapServer/3/query?f=json&where=HABITATCODE LIKE '%${code_2000.toUpperCase()}%'&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=SPECIESCODE,SITECODE,HABITATCODE,OBJECTID&outSR=102100`,
   );
 }
 
--- src/components/manage/Blocks/SpeciesProtectedSites/index.js
+++ src/components/manage/Blocks/SpeciesProtectedSites/index.js
@@ -3,7 +3,7 @@ import getSchema from './schema';
 
 export function getSpeciesProtectedSitesURL(code_2000) {
   return encodeURI(
-    `https://bio.discomap.eea.europa.eu/arcgis/rest/services/ProtectedSites/EUNIS_Website_Dyna_WM/MapServer/5/query?f=json&where=SPECIESCODE LIKE '%${code_2000.toUpperCase()}%'&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=SPECIESCODE,SITECODE,HABITATCODE,OBJECTID&outSR=102100`,
+    `https://bio.discomap.eea.europa.eu/arcgis/rest/services/ProtectedSites/EUNIS_Website_Dyna_WM/MapServer/7/query?f=json&where=SPECIESCODE LIKE '%${code_2000.toUpperCase()}%'&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=SPECIESCODE,SITECODE,HABITATCODE,OBJECTID&outSR=102100`,
   );
 }
 
