498e9955882bba9c2c549ab29dce5b988f36f489
Miu Razvan <miu.razvan28@gmail.com>
2024-12-18 14:58:16 +0200

    Make site habitats list work la species list

---
+++ src/components/manage/Blocks/SiteHabitatsList/Filters/HabitatsGroups.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+import cx from 'classnames';
+
+const HabitatsGroups = (props) => {
+  const [habitatsGroups, setHabitatsGroups] = React.useState([]);
+  const {
+    provider_data = {},
+    activeHabitatsGroup = 'All',
+    setActiveHabitatsGroup = () => {},
+  } = props;
+
+  React.useEffect(() => {
+    setHabitatsGroups(
+      [
+        ...(provider_data.habitat_group?.length ? ['All'] : []),
+        ...new Set(provider_data.habitat_group || []),
+      ].sort(),
+    );
+    /* eslint-disable-next-line */
+  }, [JSON.stringify(provider_data)]);
+
+  return (
+    <div className="habitats-groups">
+      <Container>
+        {habitatsGroups.map((species) => (
+          <button
+            key={`group-filter-${species}`}
+            className={cx({
+              'habitats-group': true,
+              active: activeHabitatsGroup === species,
+            })}
+            onClick={() => {
+              setActiveHabitatsGroup(species);
+            }}
+          >
+            {species}
+          </button>
+        ))}
+      </Container>
+    </div>
+  );
+};
+
+export default HabitatsGroups;
--- src/components/manage/Blocks/SiteHabitatsList/View.jsx
+++ src/components/manage/Blocks/SiteHabitatsList/View.jsx
@@ -1,104 +1,234 @@
 import React from 'react';
-import findIndex from 'lodash/findIndex';
-import { Icon } from '@plone/volto/components';
 import { Link } from 'react-router-dom';
-import cx from 'classnames';
-import { getObjectByIndex } from '@eeacms/volto-n2k/helpers';
-import downKeySVG from '@plone/volto/icons/down-key.svg';
-import upKeySVG from '@plone/volto/icons/up-key.svg';
+import { Container, Pagination, Grid } from 'semantic-ui-react';
+import { getObjectByIndex, photoPlaceholders } from '@eeacms/volto-n2k/helpers';
+import { Filters } from './Filters';
+import { getPopulationString, getLabelString } from './utils';
+
 import './style.less';
 
+const getCurrentPageLength = (pagination, arr) => {
+  const totalPages = Math.ceil(pagination.totalItems / pagination.itemsPerPage);
+  if (arr.length < pagination.itemsPerPage) return arr.length;
+  if (totalPages === pagination.activePage) {
+    return arr.length - (pagination.activePage - 1) * pagination.itemsPerPage;
+  }
+  return pagination.itemsPerPage;
+};
+
 const View = (props) => {
-  const [habitats, setHabitats] = React.useState({});
-  const [expandedHabitats, setExpandedHabitats] = React.useState([]);
-  const { provider_data = {}, placeholder = 'No results' } = props;
+  const {
+    provider_data = {},
+    placeholder = 'This site does not host any protected habitats',
+  } = props;
+  const dataReady = React.useRef(false);
+  const [activeHabitatsGroup, setActiveHabitatsGroup] = React.useState('All');
+  const [filters, setFilters] = React.useState({});
+  const [habitats, spetHabitats] = React.useState([]);
+  const [filteredHabitats, setFilteredHabitats] = React.useState([]);
+  const [pagination, setPagination] = React.useState({
+    activePage: 1,
+    totalItems: 0,
+    itemsPerPage: 10,
+  });
+  const [sortBy, setSortBy] = React.useState(['scientific_name', 'ASC']);
+
+  const getSortedHabitats = (habitats = []) => {
+    const property = sortBy[0];
+    const order = sortBy[1];
+
+    return habitats.sort((a, b) => {
+      const a_value = a[0][property];
+      const b_value = b[0][property];
+      if (a_value < b_value) return order === 'ASC' ? -1 : 1;
+      if (a_value > b_value) return order === 'ASC' ? 1 : -1;
+      return 0;
+    });
+  };
+
+  const getfilteredHabitats = (habitats = []) => {
+    const activeFilters = {
+      ...filters,
+      ...(activeHabitatsGroup !== 'All'
+        ? { habitat_group: [activeHabitatsGroup] }
+        : {}),
+    };
+
+    const filteredHabitats = habitats.filter((items, index) => {
+      let itemsHaveFilter = true;
+      Object.keys(activeFilters).forEach((filter) => {
+        let habitatHasFilter = false;
+        items.forEach((item) => {
+          if (activeFilters[filter].includes(item[filter])) {
+            habitatHasFilter = true;
+          }
+        });
+        if (!habitatHasFilter) {
+          itemsHaveFilter = false;
+        }
+      });
+      return itemsHaveFilter;
+    });
+
+    setPagination({
+      ...pagination,
+      activePage: 1,
+      totalItems: filteredHabitats.length,
+    });
+
+    return filteredHabitats;
+  };
 
   React.useEffect(() => {
-    const newHabitats = {};
-    if (provider_data?.habitat_group?.length) {
-      provider_data.habitat_group.forEach((habitat, index) => {
-        if (!newHabitats[habitat]) {
-          newHabitats[habitat] = [];
+    dataReady.current = false;
+    const habitatsIndex = {};
+    const newHabitats = [];
+    if (provider_data.scientific_name?.length) {
+      provider_data.scientific_name.forEach((_, index) => {
+        const habitat = getObjectByIndex(provider_data, index);
+        if (!(habitatsIndex[habitat.code_2000] >= 0)) {
+          habitatsIndex[habitat.code_2000] = index;
         }
-        newHabitats[habitat].push(getObjectByIndex(provider_data, index));
+        if (!newHabitats[habitatsIndex[habitat.code_2000]]) {
+          newHabitats[habitatsIndex[habitat.code_2000]] = [];
+        }
+        newHabitats[habitatsIndex[habitat.code_2000]].push(habitat);
       });
     }
-    setHabitats(newHabitats);
+    spetHabitats(getSortedHabitats(newHabitats.filter((habitats) => habitats)));
     /* eslint-disable-next-line */
   }, [JSON.stringify(provider_data)]);
 
-  return (
-    <div className="site-habitats-list">
-      {Object.keys(habitats)?.length ? (
-        Object.keys(habitats)
-          .sort((a, b) => a.localeCompare(b))
-          .map((habitat) => {
-            const expanded = expandedHabitats.includes(habitat);
-            return (
-              <div className="habitat" key={habitat}>
-                <div
-                  className={cx({
-                    'habitat-toolbar': true,
-                    marginless: !expanded,
-                  })}
-                >
-                  <div className="habitat-name">
-                    <h3>{habitat}</h3>
-                    <p className="count">{habitats[habitat].length}</p>
-                  </div>
-                  <Icon
-                    name={expanded ? upKeySVG : downKeySVG}
-                    onClick={(e) => {
-                      const index = findIndex(
-                        expandedHabitats,
-                        (name) => name === habitat,
-                      );
-                      if (index === -1) {
-                        setExpandedHabitats((prevExpandedHabitats) => [
-                          ...prevExpandedHabitats,
-                          habitat,
-                        ]);
-                      } else {
-                        setExpandedHabitats((prevExpandedHabitats) => {
-                          const newExpandedHabitats = [...prevExpandedHabitats];
-                          newExpandedHabitats.splice(index, 1);
-                          return newExpandedHabitats;
-                        });
-                      }
-                      e.preventDefault();
-                      e.stopPropagation();
-                    }}
-                    color="#8C8C8C"
-                    size="32px"
-                  />
-                </div>
+  React.useEffect(() => {
+    const filteredHabitats = getSortedHabitats(
+      getfilteredHabitats([...habitats]),
+    );
+    setFilteredHabitats(filteredHabitats);
+    /* eslint-disable-next-line */
+  }, [habitats, sortBy, activeHabitatsGroup, filters]);
 
-                {expanded
-                  ? habitats[habitat].map((item, index) => (
-                      <div
-                        className="habitat-item"
-                        key={`${habitat}-${index}-item`}
-                      >
-                        <Link
-                          className="description"
-                          to={`/habitats/${item.code_2000}`}
-                        >
-                          {item.habitat_description} ({item.code_2000})
-                        </Link>
-                        <p className="coverage">
-                          {item.coverage_ha.toFixed(2)} ha (
-                          {(item.coverage_ha / 100).toFixed(4)} km
-                          <sup>2</sup>)
+  return (
+    <div className="site-habitats-list full-width">
+      {props.mode !== 'edit' && habitats.length ? (
+        <Filters
+          {...props}
+          activeHabitatsGroup={activeHabitatsGroup}
+          activeFilters={filters}
+          filteredHabitats={filteredHabitats}
+          pagination={pagination}
+          sortBy={sortBy}
+          habitats={habitats}
+          setActiveHabitatsGroup={setActiveHabitatsGroup}
+          setActiveFilters={setFilters}
+          setPagination={setPagination}
+          setSortBy={setSortBy}
+        />
+      ) : (
+        ''
+      )}
+
+      <div className="habitats-list">
+        <Container>
+          {filteredHabitats.length ? (
+            Array.from(
+              {
+                length: getCurrentPageLength(pagination, filteredHabitats),
+              },
+              (v, k) =>
+                k + (pagination.activePage - 1) * pagination.itemsPerPage,
+            ).map((index) => {
+              const habitatsData = filteredHabitats[index][0];
+
+              return (
+                <Grid
+                  className="habitats"
+                  key={`${index}-${habitatsData.code_2000}`}
+                  columns="12"
+                >
+                  <Grid.Row>
+                    <Grid.Column
+                      mobile={12}
+                      tablet={3}
+                      computer={2}
+                      className="habitats-photo"
+                    >
+                      <img
+                        src={
+                          habitatsData.picture_url ||
+                          photoPlaceholders[habitatsData.habitat_group] ||
+                          photoPlaceholders.Birds
+                        }
+                        alt={habitatsData.habitat_group}
+                      />
+                    </Grid.Column>
+                    <Grid.Column
+                      mobile={12}
+                      tablet={9}
+                      computer={10}
+                      className="habitats-details"
+                    >
+                      <div className="metadata">
+                        <div className="name">
+                          <Link
+                            as="h3"
+                            to={`/habitats/${habitatsData.code_2000}`}
+                          >
+                            {habitatsData.scientific_name
+                              ? habitatsData.scientific_name + ' '
+                              : ''}
+                            <span className="code-2000">
+                              {' '}
+                              - {habitatsData.code_2000 || 'NA'}
+                            </span>
+                          </Link>
+                        </div>
+                        {filteredHabitats[index].map((habitat, index) => (
+                          <p
+                            className="habitat-data blue"
+                            style={{ fontSize: '1.2rem' }}
+                            key={`habitat-${index}-${habitat.code_2000}`}
+                          >
+                            Cover: {habitat.coverage_ha.toFixed(2)} ha (
+                            {(habitat.coverage_ha / 100).toFixed(4)} km²)
+                            {habitat.habitat_prioriy
+                              ? `; Priority habitat type: ${habitat.habitat_prioriy}`
+                              : ''}
+                          </p>
+                        ))}
+                      </div>
+                      <div className="footer-metadata">
+                        <p className="green">
+                          Appears in {habitatsData.number_sites} sites
                         </p>
                       </div>
-                    ))
-                  : ''}
-              </div>
-            );
-          })
-      ) : (
-        <p>{placeholder}</p>
-      )}
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              );
+            })
+          ) : habitats.length ? (
+            <div className="empty">No results</div>
+          ) : (
+            ''
+          )}
+          {!habitats?.length ? <div className="empty">{placeholder}</div> : ''}
+          {pagination.totalItems > 0 ? (
+            <Pagination
+              activePage={pagination.activePage}
+              totalPages={Math.ceil(
+                pagination.totalItems / pagination.itemsPerPage,
+              )}
+              onPageChange={(e, data) => {
+                setPagination({ ...pagination, activePage: data.activePage });
+              }}
+              prevItem={null}
+              nextItem={null}
+            />
+          ) : (
+            ''
+          )}
+        </Container>
+      </div>
     </div>
   );
 };
