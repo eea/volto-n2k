7570ec3ee87c9d7e13f96bc0ec17edef48977d4e
vladcalin-edw <calin.vlad@eaudeweb.ro>
2025-05-19 18:42:32 +0300

    fix lint issue

--- src/components/manage/Blocks/SiteProtectedHabitats/View.jsx
+++ src/components/manage/Blocks/SiteProtectedHabitats/View.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container } from 'semantic-ui-react';
 import { getObjectByIndex } from '@eeacms/volto-n2k/helpers';
 
