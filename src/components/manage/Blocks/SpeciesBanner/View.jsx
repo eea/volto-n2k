b207555ee533cce97f4f4d2d22a7eb84240dd76f
vladcalin-edw <calin.vlad@eaudeweb.ro>
2025-05-19 19:21:47 +0300

    fix sonarqube

--- src/components/manage/Blocks/SpeciesBanner/View.jsx
+++ src/components/manage/Blocks/SpeciesBanner/View.jsx
@@ -24,7 +24,7 @@ const getSource = (source) => {
   return parsedSource;
 };
 
-const _View = (props) => {
+const ViewComponent = (props) => {
   const swiperEl = useRef();
   const previewEl = useRef();
   const [activeSlide, setActiveSlide] = useState(0);
@@ -198,7 +198,7 @@ const _View = (props) => {
   );
 };
 
-const View = compose(
+const ViewCompose = compose(
   connectToMultipleProviders((props) => ({
     providers: [
       {
@@ -207,12 +207,12 @@ const View = compose(
       { provider_url: props.data?.species_pictures_provider },
     ],
   })),
-)(_View);
+)(ViewComponent);
 
-export default function $View(props) {
+export default function View(props) {
   return (
     <VisibilitySensor Placeholder={() => <div>loading....&nbsp;</div>}>
-      <View {...props} />
+      <ViewCompose {...props} />
     </VisibilitySensor>
   );
 }
