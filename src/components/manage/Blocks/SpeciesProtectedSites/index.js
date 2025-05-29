7767a33c7df4182f306d2549b04549b4cf5628b7
vladcalin-edw <calin.vlad@eaudeweb.ro>
2025-05-29 17:44:26 +0300

    in progress

--- src/components/manage/Blocks/SpeciesProtectedSites/index.js
+++ src/components/manage/Blocks/SpeciesProtectedSites/index.js
@@ -1,10 +1,18 @@
 import SpeciesProtectedSitesView from './View';
 import getSchema from './schema';
 
-export function getSpeciesProtectedSitesURL(code_2000) {
-  return encodeURI(
-    `https://bio.discomap.eea.europa.eu/arcgis/rest/services/ProtectedSites/EUNIS_Website_Dyna_WM/MapServer/7/query?f=json&where=SPECIESCODE LIKE '%${code_2000.toUpperCase()}%'&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=SPECIESCODE,SITECODE,HABITATCODE,OBJECTID&outSR=102100`,
+export function getSpeciesProtectedSitesURL(code_2000, layers) {
+  const baseUrl =
+    'https://bio.discomap.eea.europa.eu/arcgis/rest/services/ProtectedSites/EUNIS_Website_Dyna_WM/MapServer';
+  const queryParams = encodeURI(
+    `?f=json&where=SPECIESCODE LIKE '%${code_2000.toUpperCase()}%'&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=SPECIESCODE,SITECODE,HABITATCODE,OBJECTID&outSR=102100`,
   );
+
+  console.log('Layers: ', layers);
+  return layers.reduce((acc, layer) => {
+    acc[`layer${layer}`] = `${baseUrl}/${layer}/query${queryParams}`;
+    return acc;
+  }, {});
 }
 
 export default function applyConfig(config) {
